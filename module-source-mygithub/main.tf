c53eba6d109dbc240778bf3b594029a7eb324e44
digzect <digzect@gmail.com>
2024-07-14 03:31:21 +0900

    update

--- module-source-mygithub/main.tf
+++
@@ -1,9 +0,0 @@
-# main.tf
-provider "aws" {
-  region = "ap-southeast-1"  
-}
-
-module "ec2_seoul" {
-  source = "github.com/digzect/terraform-module-repo/terraform-aws-ec2"
-  instance_type = "t3.small"
-}
